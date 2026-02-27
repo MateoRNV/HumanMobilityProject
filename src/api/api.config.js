3d83b1a1497e92b4664c20293956a7b4f103f8ef
MateoRNV <m.narvaez2207@gmail.com>
2026-02-27 07:54:57 +0000

    Correcion api endpoint

--- src/api/api.config.js
+++ src/api/api.config.js
@@ -5,8 +5,8 @@
 
 // Revisar las APIS
 
-const API_BASE_URL = import.meta.env?.VITE_API_BASE_URL ?? "http://localhost:3001/api";
-// const API_BASE_URL = import.meta.env?.VITE_API_BASE_URL ?? 'https://wandering-lil-human-mobility-b37b6d7c.koyeb.app/api';
+// const API_BASE_URL = import.meta.env?.VITE_API_BASE_URL ?? "http://localhost:3001/api";
+const API_BASE_URL = import.meta.env?.VITE_API_BASE_URL ?? 'https://wandering-lil-human-mobility-b37b6d7c.koyeb.app/api';
 // const API_BASE_URL = import.meta.env?.VITE_API_BASE_URL ?? 'https://human-mobility-backend.onrender.com/api';
 
 const defaultHeaders = {
